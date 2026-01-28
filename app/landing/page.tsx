426f1db5b811d693dd0acdfd9d97e8555634ffda
HAWBAR0IQ <carlabihawbar434@gmail.com>
2026-01-28 21:52:32 +0200

    Front Ui Creation / Edits

---
+++ app/landing/page.tsx
@@ -0,0 +1,36 @@
+import Championship from '@/components/landing/Championship';
+import DriversSpotlight from '@/components/landing/DriversSpotlight';
+import Footer from '@/components/landing/Footer';
+import Hero from '@/components/landing/Hero';
+import Navbar from '@/components/landing/Navbar';
+import React from 'react';
+
+
+const LandingPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-white">
+
+      <Navbar />
+      
+      <main className="flex-grow">
+        
+        <section id="hero">
+           <Hero />
+        </section>
+
+        <section id="driversspotlight">
+           <DriversSpotlight />
+        </section>
+
+        <section id="championship">
+           <Championship />
+        </section>
+
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
